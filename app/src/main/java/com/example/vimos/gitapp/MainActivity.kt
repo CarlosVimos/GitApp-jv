da66104435080783b9b51bca8643b1cb01821107
Karol Gruetzmacher <vimos.app@gmail.com>
2018-06-20 00:51:02 +0200

    test

--- app/src/main/java/com/example/vimos/gitapp/MainActivity.kt
+++ app/src/main/java/com/example/vimos/gitapp/MainActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    var String = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
